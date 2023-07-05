d197f04db8cb7a6ab732871057dd345fda1068ae
Julien Bogiraud <julien@bogiraud.com>
2023-07-05 18:16:11 +0200

    lifecicle

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,30 +1,41 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, OnDestroy, OnInit } from '@angular/core';
+import {of} from 'rxjs'
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit, OnDestroy, AfterViewInit {
+  boats : Array;
+
+  constructor() { }
+  ngAfterViewInit(): void {
+    throw new Error('Method not implemented.');
+  }
+  ngOnDestroy(): void {
+    throw new Error('Method not implemented.');
+  }
+  ngOnInit(): void {
+    this.boats = of([
+      {
+      name: 'Starfire',
+      year: 1997,
+      img: "assets/boat.jpg"
+    },{
+      name: 'Starfire2',
+      year: 1999,
+      img: "assets/boat.jpg"
+    },{
+      name: 'Starfire3',
+      year: 2000,
+      img: "assets/boat.jpg"
+    },
+  ]);
+  }
+
   clicked = false;
   title ='this is a title';
-
-// Boat Data
-  boats = [
-    {
-    name: 'Starfire',
-    year: 1997,
-    img: "assets/boat.jpg"
-  },{
-    name: 'Starfire2',
-    year: 1999,
-    img: "assets/boat.jpg"
-  },{
-    name: 'Starfire3',
-    year: 2000,
-    img: "assets/boat.jpg"
-  },
-]
 
   handleClick() {
     this.clicked = true;
